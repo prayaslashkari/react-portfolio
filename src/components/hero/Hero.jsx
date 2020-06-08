d75e5485a89dd37e8b438df7251ff7da58154fe4
unknown <rock198269@gmail.com>
2020-06-09 01:37:51 +0530

    name change

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     return ( 
         <div className={styles.container}>
             <div className={styles.inner}>
-                <h1 className={styles.title}>Hi, I'm Prayas. Nice to meet you. </h1>
+                <h1 className={styles.title}>Hi, I'm Monky. Nice to meet you. </h1>
                 <h2 className={styles.subtitle}>Lorem ipsum dolor sit, amet consectetur adipisicing elit.Quod repellendus aut accusamus hic adipisci quia atque velit non quis ipsum ipsa veniam accusantium eaque voluptatem omnis soluta esse incidunt nesciunt molestias nemo, voluptatum aperiam eligendi? Et modi magni ipsum magnam quam illum fuga? Est quod aliquid ea similique officiis?</h2>
             </div>
         
